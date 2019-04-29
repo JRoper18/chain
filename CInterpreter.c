d233ed1fc456f57c59623e3efb6168e9ab9ef02a
John MacMichael <Jpmac73@gmail.com>
2019-04-29 13:39:30 -0500

    started CInterpreter.c, separates modifiers from function names.

---
+++ CInterpreter.c
@@ -0,0 +1,152 @@
+//
+// Created by John on 4/27/2019.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Parameter for a function,
+ * includes the type, name, and
+ * functions which feed it.
+ */
+typedef struct params
+{
+    char* type;
+    char* name;
+    char** funcs [];
+    bool isAtomic;
+} param;
+
+/* Stores information about a
+ * given function, including
+ * type, name, and a list of
+ * parameters.
+ */
+typedef struct functionInfo
+{
+    char* modifiers;
+    char* name;
+    param* parameters;
+} functionInfo;
+
+/* Reads through a chain file
+ * to find the information
+ * required to convert chain
+ * code to c.
+ */
+functionInfo* interpret (char* fileName)
+{
+    FILE* file = fopen (fileName, "r");
+
+    if (file == NULL)
+    {
+        printf ("Error while reading file: %s.\n", fileName);
+        exit (1);
+    }
+
+    char n = '\0';
+    char* word = (char*)malloc (2000 * sizeof (char));
+    int letterNum = 0;
+    functionInfo* info = (functionInfo*)malloc (100 * sizeof (functionInfo));
+    int infoNum = 0;
+
+    int state = 0;
+    /* STATE OF THE PROGRAM
+     * 0 - findType
+     * 1 - findName
+     * 2 - findParams
+     * 3 - find{
+     * 4 - traverse
+     * 5 - find=
+     * 6 - find;
+     */
+
+    bool oneLineComment = 0;
+    bool multiLineComment = 0;
+    char prev = '\0';
+    while ((n = fgetc (file)) != EOF)
+    {
+        if (prev == '/' && n == '*')
+        {
+            multiLineComment = true;
+            if (state == 0)
+            {
+                letterNum--;
+                word [letterNum] = '\0';
+            }
+        }
+        else if (prev == '/' && n == '/')
+        {
+            oneLineComment = true;
+            if (state == 0)
+            {
+                letterNum--;
+                word [letterNum] = '\0';
+            }
+        }
+
+        if (oneLineComment)
+        {
+            if (n == '\n')
+            {
+                oneLineComment = false;
+            }
+        }
+        else if (multiLineComment)
+        {
+            if (prev == '*' && n == '/')
+            {
+                oneLineComment = false;
+                multiLineComment = false;
+            }
+        }
+        else
+        {
+            // findName - finds the return type of the function.
+            if (state == 0)
+            {
+                if (n != '(')
+                {
+                    word [letterNum] = n;
+                    letterNum++;
+                    word [letterNum] = '\0';
+                }
+                else if (n == ';')
+                {
+                    letterNum = 0;
+                    word [letterNum] = 0;
+                }
+                else if (n != '=')
+                {
+                    info [infoNum] = (functionInfo*)malloc (sizeof (functionInfo));
+                    info [infoNum].modifiers = (char*)malloc (2000 * sizeof (char));
+                    char* idx = findLastWord (infoNum);
+                    if (*idx == '*')
+                    {
+                        *(idx - 1) = '*';
+                        *idx = '\0'
+                        idx++;
+                    }
+                    info [infoNum].name = (char*)malloc ((strlen (word) - (word - idx)) * sizeof (char));
+                    strcpy (info [infoNum].name, idx);
+                    strcpy (info [infoNum].modifiers, word);
+                    letterNum = 0;
+                    word [letterNum] = '\0';
+                    state = 1;
+                }
+                else
+                {
+                    /*
+                     * TODO
+                     * ----
+                     * Atomic variables.
+                     */
+                }
+            }
+        }
+        prev = n;
+    }
+
+    return info;
+}
