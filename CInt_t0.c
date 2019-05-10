cee21d4b90acafac7cb86902e9b78610705d48c0
John MacMichael <Jpmac73@gmail.com>
2019-05-10 10:19:36 -0500

    Worked on alternate.spl

--- CInt_t0.c
+++ CInt_t0.c
@@ -1,24 +1,25 @@
 #include "lib.h" 
 Function** functions;
 
-void mod (int* array, int index, int modulo);
-void mod_async (int* array, int index, int modulo)
+int* final;
+void mod (int index);
+void mod_async (int index)
 {
-	Value argsIn[3] = {asPointer (array),asInt (index),asInt (modulo)};
+	Value argsIn[1] = {asInt (index)};
 	executeFunction (functions [0], argsIn);
 }
 
 Value mod_HELPER_D82KT6KF9 (Value* inputArgs)
 {
-	mod (inputArgs[0].asInt , inputArgs[1].asPointer , inputArgs[2].asInt);
+	mod (inputArgs[0].asInt);
 }
 
-void mod(int* array, int index, int modulo)
+void mod(int index)
 {
-    array [index] = array [index] % modulo;
+    final [index] = final [index] % 1000;
 }
-void add (int* array, int i1, int i2);
-void add_async (int* array, int i1, int i2)
+int add (int* array, unsigned int i1, unsigned int i2);
+void add_async (int* array, unsigned int i1, unsigned int i2)
 {
 	Value argsIn[3] = {asPointer (array),asInt (i1),asInt (i2)};
 	executeFunction (functions [1], argsIn);
@@ -26,15 +27,15 @@ void add_async (int* array, int i1, int i2)
 
 Value add_HELPER_D82KT6KF9 (Value* inputArgs)
 {
-	add (inputArgs[0].asInt , inputArgs[1].asPointer , inputArgs[2].asInt);
+	return asInt (add (inputArgs[0].asInt , inputArgs[1].asPointer , inputArgs[2].asInt));
 }
 
-void add(int* array, int i1, int i2)
+int add(int* array, unsigned int i1, unsigned int i2)
 {
-    array [i1] = array [i1] + array [i2];
-    mod (array, i1, 1000);
+    array [i1] = array [i1 % 1000] + array [i2 % 1000];
+
+    return i1;
 }
-int* final;
 int done;
 int main (int argc, char** argv);
 void main_async (int argc, char** argv)
@@ -53,22 +54,23 @@ int main(int argc, char** argv)
 	initPool ();
 	functions = calloc(100, sizeof(Function*));
 
-	functions [0] = makeFunction (3, mod_HELPER_D82KT6KF9);
+	functions [0] = makeFunction (1, mod_HELPER_D82KT6KF9);
 	functions [1] = makeFunction (3, add_HELPER_D82KT6KF9);
-    int* array = (int*)malloc (100 * sizeof (int));
+	waitFor (functions [1], functions [0], 0);
+    final = (int*)malloc (100 * sizeof (int));
     printf ("GENERATING RANDOM NUMBERS!\n");
     for (int i = 0; i < 1000; i++)
     {
-        array [i] = 1000 - i - 1;
+        final [i] = 1000 - i - 1;
     }
     for (int i = 0; i < 100000; i++)
     {
-        add_async (array, i, array [i]);
+        add_async (final, i, final [i]);
     }
 
     for (int i = 0; i < 1000; i++)
     {
-        printf ("%d\n", array [i]);
+        printf ("%d\n", final [i]);
     }
 	finish ();
 }
